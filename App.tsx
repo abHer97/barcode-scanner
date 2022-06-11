afd22c4deb1c9a765e1814986db211ec944131a7
abel herrera <ahpard97@gmail.com>
2022-06-11 02:01:12 -0500

    update App to add scanner component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Scanner } from './src/components/Scanner';
 
 export default function App() {
    return (
-      <View style={styles.container}>
-         <Text>Open up App.tsx to start working on your app!</Text>
-         <StatusBar style='auto' />
-      </View>
+      <Scanner />
+      // <View style={styles.container}>
+      //    <Text>Open up App.tsx to start working on your app!</Text>
+      //    <StatusBar style='auto' />
+      // </View>
    );
 }
 
